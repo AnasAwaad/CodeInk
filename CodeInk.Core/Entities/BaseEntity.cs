7837d0b3bd6e338bc8ffc1363e19c1e014943e1d
anas awaad <anas.shaban.awaad@gmail.com>
2024-11-29 15:54:04 +0200

    update baseEngity table

--- CodeInk.Core/Entities/BaseEntity.cs
+++ CodeInk.Core/Entities/BaseEntity.cs
@@ -1,5 +1,13 @@
 ﻿namespace CodeInk.Core.Entities;
 public class BaseEntity
 {
+    public BaseEntity()
+    {
+        IsActive = true;
+        CreatedOn = DateTime.Now;
+    }
     public int Id { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime? CreatedOn { get; set; }
+    public DateTime? LastUpdatedOn { get; set; }
 }
