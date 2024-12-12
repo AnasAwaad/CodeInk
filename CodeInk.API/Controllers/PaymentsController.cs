7fb633edc1ff98b130b40d294fa7a30bf838c1af
anas awaad <anas.shaban.awaad@gmail.com>
2024-12-12 02:18:52 +0200

    add webhook end point and test it locally

--- CodeInk.API/Controllers/PaymentsController.cs
+++ CodeInk.API/Controllers/PaymentsController.cs
@@ -1,10 +1,13 @@
 ﻿using CodeInk.API.Errors;
 using CodeInk.Service.DTOs.Payment;
 using CodeInk.Service.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
 
 namespace CodeInk.API.Controllers;
 
+[Authorize(Roles = "Customer")]
 public class PaymentsController : APIBaseController
 {
     private readonly IPaymentService _paymentService;
@@ -20,5 +23,34 @@ public class PaymentsController : APIBaseController
     {
         var result = await _paymentService.CreateOrUpdatePaymentIntent(paymentCart);
         return Ok(new ApiResponse(201, "Payment intent created successfully", result));
+    }
+
+    [HttpPost("webhook")]
+    public async Task<IActionResult> StripeWebHook()
+    {
+        var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+        const string endpointSecret = "whsec_34d7d3c08a44698b2cfda012f1fe3a2c971e73be8acd05195e411fdae6dbe5ae";
+
+        var stripeEvent = EventUtility.ParseEvent(json);
+        var signatureHeader = Request.Headers["Stripe-Signature"];
+
+        stripeEvent = EventUtility.ConstructEvent(json,
+                signatureHeader, endpointSecret);
+
+        if (stripeEvent.Type == EventTypes.PaymentIntentSucceeded)
+        {
+            var paymentIntent = stripeEvent.Data.Object as PaymentIntent;
+            await _paymentService.UpdateOrderPaymentSucceeded(paymentIntent.Id);
+
+        }
+        else if (stripeEvent.Type == EventTypes.PaymentIntentPaymentFailed)
+        {
+            var paymentIntent = stripeEvent.Data.Object as PaymentIntent;
+
+            await _paymentService.UpdateOrderPaymentFailed(paymentIntent.Id);
+        }
+
+        return new EmptyResult();
+
     }
 }
